bb0be72f8eaa2afac8bb942a16e3287ffc3fc6bd
fyrux <fyrux@DESKTOP-S5QH42O>
2024-06-14 17:18:34 +0200

    Ajoutez des fichiers projet.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Biathlon_Esthivaukan
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Biathlon_Esthivaukan
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

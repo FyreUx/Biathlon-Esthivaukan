f5980afcb05054f237c83402d9abec3b89b002f4
FyreUx <fyruxyt@gmail.com>
2024-06-14 23:23:36 +0200

    ajout page run

--- Runpage.xaml.cs
+++ Runpage.xaml.cs
@@ -1,9 +1,51 @@
+using System;
+using Microsoft.Maui.Controls;
+
 namespace Biathlon_Esthivaukan;
 
 public partial class Runpage : ContentPage
 {
-	public Runpage()
+    bool isRunning = true;
+    DateTime startTime;
+    TimeSpan elapsedTime;
+
+    public Runpage()
 	{
 		InitializeComponent();
+        StartCounter();
 	}
+
+    private async void StartCounter()
+    {
+        startTime = DateTime.Now;
+
+        while (isRunning)
+        {
+            TimeSpan elapsed = DateTime.Now - startTime;
+            StopwatchLabel.Text = elapsed.ToString(@"mm\:ss");
+
+            // Stop the counter after one hour
+            if (elapsed >= TimeSpan.FromMinutes(60))
+            {
+                isRunning = false;
+            }
+
+            await Task.Delay(1000); // Wait for 1 second
+        }
+    }
+
+
+    private async void OnShootClicked(object sender, EventArgs e)
+    {
+        isRunning = false; // Stop the timer
+
+        // Store the elapsed time in a public variable
+        PublicVariables.ElapsedTime = elapsedTime;
+        await Navigation.PushAsync(new Shootpage());
+    }
+
+    public static class PublicVariables
+    {
+        public static TimeSpan ElapsedTime { get; set; }
+    }
 }

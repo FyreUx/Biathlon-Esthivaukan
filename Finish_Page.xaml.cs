bb73538caafdf4d82660b61a1755928f9231b505
FyreUx <fyruxyt@gmail.com>
2024-06-15 14:30:50 +0200

    mail auto 1er jet + des trucs

--- Finish_Page.xaml.cs
+++ Finish_Page.xaml.cs
@@ -1,3 +1,8 @@
+using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+
+
 namespace Biathlon_Esthivaukan;
 
 public partial class Finish_Page : ContentPage
@@ -6,4 +11,47 @@ public partial class Finish_Page : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private async void OnShareClicked(object sender, EventArgs e)
+    {
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress("Your Name", "biathlon.vaukan@gmail.com"));
+        message.To.Add(new MailboxAddress("Recipient Name", "fyruxyt@gmail.com"));
+        message.Subject = "subject";
+
+        message.Body = new TextPart("plain")
+        {
+            Text = "body"
+        };
+
+        using (var client = new SmtpClient())
+        {
+            try
+            {
+                // For demo-purposes, accept all SSL certificates (in case the server supports STARTTLS)
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                await client.ConnectAsync("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+
+                // Note: only needed if the SMTP server requires authentication
+                await client.AuthenticateAsync("biathlon.vaukan@gmail.com", "ftqv dilq epmj vgbt");
+
+                await client.SendAsync(message);
+                await client.DisconnectAsync(true);
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception (e.g., display an error message)
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+
+    private async void OnHomeClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MainPage(), false);
+
+    }
+
 }

73395b25213b81c2bdf59ade055631232a7ecc94
erenyagert <moussaouassou@gmail.com>
2024-06-19 13:08:33 +0200

    Profil_Page mémorisation des données

--- Profil_Page.xaml.cs
+++ Profil_Page.xaml.cs
@@ -1,61 +1,76 @@
 using Microsoft.Extensions.Logging.Abstractions;
-using Microsoft.Maui.Controls; // ou using Xamarin.Forms; selon le framework utilisé
+using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.Xaml;
 
-namespace Biathlon_Esthivaukan;
-
-public partial class Profil_Page : ContentPage
+namespace Biathlon_Esthivaukan
 {
-    public Profil_Page()
+    public partial class Profil_Page : ContentPage
     {
-        InitializeComponent();
-    }
-
-
-
-    private async void OnSubmitClicked(object sender, EventArgs e)
-    {
-        if (!string.IsNullOrEmpty(PrenomEntry.Text))
+        public Profil_Page()
         {
-            string prenom = PrenomEntry.Text;
-            PublicVariablesPP.Prenom = prenom;
+            InitializeComponent();
+            LoadUserData();
         }
 
-        if (!string.IsNullOrEmpty(NomEntry.Text))
+        protected override void OnAppearing()
         {
-            string nom = NomEntry.Text;
-            PublicVariablesPP.Nom = nom;
-        }
-        if (!string.IsNullOrEmpty(EmailEntry.Text))
-        {
-            string email = EmailEntry.Text;
-            PublicVariablesPP.Email = email;
+            base.OnAppearing();
+            LoadUserData();
         }
 
-       
-        // Stockage des informations utilisateur dans les variables publiques
-        
-        
-        DisplayAlert("Information Saisie", $"Prénom: {PublicVariablesPP.Prenom}\nNom: {PublicVariablesPP.Nom}\nEmail: {PublicVariablesPP.Email}", "OK");
-        await Navigation.PushAsync(new MainPage(), false);
+        private void LoadUserData()
+        {
+            if (!string.IsNullOrEmpty(PublicVariablesPP.Prenom))
+            {
+                PrenomEntry.Text = PublicVariablesPP.Prenom;
+            }
+            if (!string.IsNullOrEmpty(PublicVariablesPP.Nom))
+            {
+                NomEntry.Text = PublicVariablesPP.Nom;
+            }
+            if (!string.IsNullOrEmpty(PublicVariablesPP.Email))
+            {
+                EmailEntry.Text = PublicVariablesPP.Email;
+            }
+        }
+
+        private async void OnSubmitClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(PrenomEntry.Text))
+            {
+                PublicVariablesPP.Prenom = PrenomEntry.Text;
+            }
+
+            if (!string.IsNullOrEmpty(NomEntry.Text))
+            {
+                PublicVariablesPP.Nom = NomEntry.Text;
+            }
+
+            if (!string.IsNullOrEmpty(EmailEntry.Text))
+            {
+                PublicVariablesPP.Email = EmailEntry.Text;
+            }
+
+            // Stockage des informations utilisateur dans les variables publiques
+            await DisplayAlert("Information Saisie", $"Prénom: {PublicVariablesPP.Prenom}\nNom: {PublicVariablesPP.Nom}\nEmail: {PublicVariablesPP.Email}", "OK");
+            await Navigation.PushAsync(new MainPage(), false);
+        }
+
+        private async void OnHomeClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage(), false);
+        }
+
+        private async void OnTrackingClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SuiviPerf(), false);
+        }
+
+        public static class PublicVariablesPP
+        {
+            public static string Prenom { get; set; }
+            public static string Nom { get; set; }
+            public static string Email { get; set; }
+        }
     }
-    public static class PublicVariablesPP
-    {
-        public static string Prenom { get; set; }
-        public static string Nom { get; set; }
-        public static string Email { get; set; }
-    }
-
-
-    private async void OnHomeClicked(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new MainPage(), false);
-
-    }
-    private async void OnTrackingClicked(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new SuiviPerf(), false);
-
-    }
-
 }

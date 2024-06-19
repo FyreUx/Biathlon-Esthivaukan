d3280ae33c751f20d72557ea16dfdb1c85846806
FyreUx <fyruxyt@gmail.com>
2024-06-19 14:14:15 +0200

    suivi perf complété avec lecture manque plus que l'appel a history avec l'user en variable

--- SuiviPerf.xaml.cs
+++ SuiviPerf.xaml.cs
@@ -2,6 +2,10 @@ namespace Biathlon_Esthivaukan;
 
 public partial class SuiviPerf : ContentPage
 {
+    public string? ButtonText1 { get; set; }
+    public string? ButtonText2 { get; set; }
+    public string? ButtonText3 { get; set; }
+    public string? ButtonText4 { get; set; }
     
     private readonly string _csvFilePath;
     public SuiviPerf()
@@ -11,25 +15,83 @@ public partial class SuiviPerf : ContentPage
         _csvFilePath = Path.Combine(FileSystem.AppDataDirectory, "user_data.csv");
         ReadUserData();
 
-    }
+}
 
     private void ReadUserData()
     {
         var csvHelper = new CSVHelper(_csvFilePath);
         var userDataList = csvHelper.ReadAllUserData();
+        int length = userDataList.Count;
 
-        foreach (var userData in userDataList)
+        var userData_1 = length > 1 ? userDataList[length -1] : null;
+        var userData_2 = length > 2 ? userDataList[length - 2] : null;
+        var userData_3 = length > 3 ? userDataList[length - 3] : null;
+        var userData_4 = length > 4 ? userDataList[length - 4] : null;
+
+        if (userData_1 != null)
         {
-            foreach (var field in userData)
-            {
-                Console.Write(field + "\t");
-            }
-            Console.WriteLine();
+            PublicVariablesSP.ud1 = userData_1;
+            ButtonText1 = $"Run n° 1 Temps Total {userData_1[4]}";
+
         }
+        else
+        {
+            Console.WriteLine("userData_1 is null");
+            Btn1.IsVisible = false;
+            Btn1.IsEnabled = false;
+
+        }
+        if (userData_2 != null)
+        {
+            PublicVariablesSP.ud2 = userData_2;
+            ButtonText2 = $"Run n° 2 Temps Total {userData_2[4]}";
+        }
+        else
+        {
+            Console.WriteLine("userData_2 is null");
+            Btn2.IsVisible = false;
+            Btn2.IsEnabled = false;
+        }
+        if (userData_3 != null)
+        {
+            PublicVariablesSP.ud3 = userData_3;
+            ButtonText3 = $"Run n° 3 Temps Total {userData_3[4]}";
+        }
+        else
+        {
+            Console.WriteLine("userData_3 is null");
+            Btn3.IsVisible = false;
+            Btn3.IsEnabled = false;
+        }
+        if (userData_4 != null)
+        {
+            PublicVariablesSP.ud4 = userData_4;
+            ButtonText4 = $"Run n° 4 Temps Total {userData_4[4]}";
+        }
+        else
+        {
+            Console.WriteLine("userData_4 is null");
+            Btn4.IsVisible = false;
+            Btn4.IsEnabled = false;
+        }
+
+        BindingContext = this;
+
     }
 
+    public static class PublicVariablesSP
+    {
+        public static List<string>? ud1 { get; set; }
 
-        private async void OnHomeClicked(object sender, EventArgs e)
+        public static List<string>? ud2 { get; set; }
+
+        public static List<string>? ud3 { get; set; }
+
+        public static List<string>? ud4 { get; set; }
+
+    }
+
+    private async void OnHomeClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new MainPage(),false);
 
